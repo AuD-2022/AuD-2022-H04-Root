2a508c22b47978db3f6b5461dd16dc1185d1b2f3
Dustin Glaser <dustin.glaser@posteo.de>
2022-05-29 12:02:28 +0200

    add test2

--- src/grader/java/h04/H1_1_1.java
+++ src/grader/java/h04/H1_1_1.java
@@ -46,9 +46,29 @@ class H1_1_1 {
                 assertEqualsApply(array1, value, expected, actual);
             }
             // without difference
-            double value = (double) i / (array1.length - 1);
-            int actual = function1.apply(value);
-            assertEqualsApply(array1, value, expected, actual);
+            double value = (double) i / (array.length - 1);
+            int actual = function.apply(value);
+            assertEqualsApply(array, value, expected, actual);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/h1/arrays")
+    void test2(@ConvertWith(IntArrayConverter.class) int[] array) {
+        var function = new ArrayDoubleToIntFunction(array);
+        int n = array.length;
+        for (double x = DELTA_TEST; x < n - 1; x += DELTA_TEST) {
+            if (abs(x - round(x)) < DELTA) {
+                continue;
+            }
+            double value = x / (n - 1);
+            int x0 = (int) floor(x);
+            int x1 = (int) ceil(x);
+            double f0 = array[x0];
+            double f1 = array[x1];
+            int expected = (int) round(f0 + ((f1 - f0) / (x1 - x0)) * (x - x0));
+            int actual = function.apply(value);
+            assertEqualsApply(array, value, expected, actual);
         }
     }
 
