6601c71d87df1a4498c444c39951d1020b3e02aa
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-02 23:20:41 +0200

    add assert calls

--- src/grader/java/h04/student/ArrayDoubleToIntFunctionStudent.java
+++ src/grader/java/h04/student/ArrayDoubleToIntFunctionStudent.java
@@ -1,19 +1,35 @@
 package h04.student;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+
 import h04.TUtils;
 import h04.function.ArrayDoubleToIntFunction;
 
 public final class ArrayDoubleToIntFunctionStudent {
 
+    private final int[] array;
     private final ArrayDoubleToIntFunction instance;
 
     public ArrayDoubleToIntFunctionStudent(int[] array) {
+        this.array = IntStream.of(array).toArray();
         this.instance = TUtils.assertImplemented(() -> new ArrayDoubleToIntFunction(array));
     }
 
-    public int apply(double value) {
-        return instance.apply(value);
+    private int apply(double value) {
+        return TUtils.assertImplemented(() -> instance.apply(value));
     }
 
 
+    public IllegalArgumentException assertThrowsApply(double value) {
+        return TUtils.assertImplemented(
+            () -> assertThrows(IllegalArgumentException.class, () -> apply(value), format("apply(%s)", value)));
+    }
+
+    public void assertEqualsApply(double value, double expected) {
+        assertEquals(expected, apply(value), format("apply(%s) differs for array %s", value, Arrays.toString(array)));
+    }
 }
