e70f38aa34853267e78c88cbb9dfd6b2c19d0b34
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-03 18:28:15 +0200

    initial version of RubricUtils

---
+++ src/grader/java/h04/RubricUtils.java
@@ -0,0 +1,24 @@
+package h04;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+
+import org.sourcegrade.jagr.api.rubric.Criterion;
+import org.sourcegrade.jagr.api.rubric.Grader;
+import org.sourcegrade.jagr.api.rubric.JUnitTestRef;
+
+public class RubricUtils {
+
+    @SafeVarargs
+    public static Criterion criterion(String description, Callable<Method>... methods) {
+        Criterion.Builder criterionBuilder = Criterion.builder();
+        criterionBuilder.shortDescription(description);
+        Grader.TestAwareBuilder grader = Grader.testAwareBuilder();
+        for (Callable<Method> m : methods) {
+            grader = grader.requirePass(JUnitTestRef.ofMethod(m));
+        }
+        grader.pointsPassedMax();
+        grader.pointsFailedMin();
+        return criterionBuilder.grader(grader.build()).build();
+    }
+}
