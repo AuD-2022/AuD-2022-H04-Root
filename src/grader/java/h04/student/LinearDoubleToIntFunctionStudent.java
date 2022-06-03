1486f62f34cb6d9cb7c277c5d7dfdc19c3cade86
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-03 15:02:36 +0200

    add LinearDoubleToIntFunctionStudent

---
+++ src/grader/java/h04/student/LinearDoubleToIntFunctionStudent.java
@@ -0,0 +1,42 @@
+package h04.student;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+
+import h04.TUtils;
+import h04.function.LinearDoubleToIntFunction;
+
+public final class LinearDoubleToIntFunctionStudent {
+
+    private final double a, b;
+    private final LinearDoubleToIntFunction instance;
+
+    public LinearDoubleToIntFunctionStudent(double a, double b) {
+        this.a = a;
+        this.b = b;
+        this.instance = TUtils.assertImplemented(() -> new LinearDoubleToIntFunction(a, b));
+    }
+
+    private int apply(double value) {
+        return TUtils.assertImplemented(() -> instance.apply(value));
+    }
+
+
+    public IllegalArgumentException assertThrowsApply(double value) {
+        return TUtils.assertImplemented(
+            () -> assertThrows(
+                IllegalArgumentException.class,
+                () -> apply(value),
+                () -> format("apply(%s) for a=%s, b=%s", value, a, b)
+            )
+        );
+    }
+
+    public void assertEqualsApply(double value, double expected) {
+        assertEquals(
+            expected,
+            apply(value),
+            () -> format("apply(%s) differs for a=%s, b=%s", value, a, b)
+        );
+    }
+}
