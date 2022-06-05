69ce070989583bebd60b39c4481ea6b071dc601c
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-05 19:13:17 +0200

    add StringListConverter

--- src/grader/java/h04/JUnitUtils.java
+++ src/grader/java/h04/JUnitUtils.java
@@ -1,6 +1,8 @@
 package h04;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -13,6 +15,18 @@ public class JUnitUtils {
         protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
             if (source instanceof String s && int[].class.isAssignableFrom(targetType)) {
                 return Arrays.stream(s.split(" ")).mapToInt(Integer::valueOf).toArray();
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static class StringListConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            if (source instanceof String s && List.class.isAssignableFrom(targetType)) {
+                return Arrays.stream(s.split(" ")).collect(Collectors.toList());
             } else {
                 throw new IllegalArgumentException();
             }
