c42caadde1a2d0fac01cc0b0864dd5265d0db9c1
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-07 16:16:19 +0200

    add reflection utils

--- src/grader/java/h04/TUtils.java
+++ src/grader/java/h04/TUtils.java
@@ -1,5 +1,7 @@
 package h04;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.concurrent.Callable;
 
 import static h04.TextConstants.NOT_IMPLEMENTED;
@@ -29,6 +31,38 @@ public class TUtils {
                 throw e;
             }
             throw new AssertionFailedError(NOT_IMPLEMENTED);
+        }
+    }
+
+    public static TutorMethod getMethod(Class<?> clazz, String name, Class<?>... types) {
+        try {
+            return new TutorMethod(clazz.getDeclaredMethod(name, types));
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class TutorMethod {
+
+        public final Method method;
+
+        public TutorMethod(Method method) {
+            this.method = method;
+        }
+
+        @SuppressWarnings("unchecked")
+        public <T> T callMethod(Object object, Object... arguments) {
+            method.setAccessible(true);
+            try {
+                return (T) method.invoke(object, arguments);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                if (e.getCause() instanceof RuntimeException re) {
+                    throw re;
+                }
+                throw new RuntimeException(e);
+            }
         }
     }
 }
