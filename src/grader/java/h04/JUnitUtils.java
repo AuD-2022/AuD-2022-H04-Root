bac047f77b203d7051914d961c831501b7401434
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-07 16:12:13 +0200

    add stream converter

--- src/grader/java/h04/JUnitUtils.java
+++ src/grader/java/h04/JUnitUtils.java
@@ -3,6 +3,7 @@ package h04;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -12,7 +13,7 @@ public class JUnitUtils {
     public static class IntArrayConverter extends SimpleArgumentConverter {
 
         @Override
-        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+        public Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
             if (source instanceof String s && int[].class.isAssignableFrom(targetType)) {
                 return Arrays.stream(s.split(" ")).mapToInt(Integer::valueOf).toArray();
             } else {
@@ -21,12 +22,25 @@ public class JUnitUtils {
         }
     }
 
+    @Deprecated
     public static class StringListConverter extends SimpleArgumentConverter {
 
         @Override
-        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+        public Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
             if (source instanceof String s && List.class.isAssignableFrom(targetType)) {
                 return Arrays.stream(s.split(" ")).collect(Collectors.toList());
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static class StreamConverter extends SimpleArgumentConverter {
+
+        @Override
+        public Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            if (source instanceof String s && Stream.class.isAssignableFrom(targetType)) {
+                return Arrays.stream(s.split(" "));
             } else {
                 throw new IllegalArgumentException();
             }
