651860c6f77ad066e6f61b65553713429a57f740
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-07 16:11:01 +0200

    use streams and new naming convention

--- src/grader/java/h04/H1_2_1.java
+++ src/grader/java/h04/H1_2_1.java
@@ -1,6 +1,8 @@
 package h04;
 
-import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 import h04.student.FunctionOnRatioOfRunsStudent;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,14 +14,14 @@ public class H1_2_1 {
     FunctionOnRatioOfRunsStudent function = new FunctionOnRatioOfRunsStudent();
 
     @ParameterizedTest
-    @CsvFileSource(resources = "h1_2/one_run")
-    public void t1(@ConvertWith(JUnitUtils.StringListConverter.class) List<String> list) {
-        function.assertEqualsApply(1, list);
+    @CsvFileSource(resources = "h1_2_1/one_run")
+    public void t1(@ConvertWith(JUnitUtils.StreamConverter.class) Stream<String> stream) {
+        function.assertEqualsApply(1, stream.collect(toList()));
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "h1_2/multiple_runs")
-    public void t2(@ConvertWith(JUnitUtils.StringListConverter.class) List<String> list, int number) {
-        function.assertEqualsApply(number, list);
+    @CsvFileSource(resources = "h1_2_1/multiple_runs")
+    public void t2(@ConvertWith(JUnitUtils.StreamConverter.class) Stream<String> list, int number) {
+        function.assertEqualsApply(number, list.collect(toList()));
     }
 }
