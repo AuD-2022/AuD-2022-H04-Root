10ef7abf3f84c45a7eeb70d4ec200ca37a1cdd12
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-08 11:30:42 +0200

    add tests for adaptiveMergeSortInPlace

--- src/grader/java/h04/H2_2.java
+++ src/grader/java/h04/H2_2.java
@@ -1,5 +1,6 @@
 package h04;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static h04.ListUtils.listItemCollector;
@@ -8,8 +9,12 @@ import static h04.ListUtils.stream;
 import static java.lang.String.format;
 import static java.util.stream.IntStream.range;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
 
 import h04.JUnitUtils.StreamConverter;
+import h04.collection.ListItem;
 import h04.student.MyCollectionsStudent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -26,19 +31,25 @@ public class H2_2 {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "h2_2/two_runs")
+    @CsvFileSource(resources = "h2_2/split_two_runs")
     public void t1(@ConvertWith(StreamConverter.class) Stream<String> list, int index) {
         assertCorrectSplit(list, index);
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "h2_2/multiple_runs")
+    @CsvFileSource(resources = "h2_2/split_multiple_runs")
     public void t2(@ConvertWith(StreamConverter.class) Stream<String> list, int index) {
         assertCorrectSplit(list, index);
     }
 
-    public void assertCorrectSplit(Stream<String> stream, int firstIndexSecond) {
-        var list = stream.toList();
+    @ParameterizedTest
+    @CsvFileSource(resources = "h2_2/adaptiveMergeSortInPlace_anchor")
+    public void t3(@ConvertWith(StreamConverter.class) Stream<String> list, int threshold) {
+        assertAnchorAdaptiveMergeSortInPlace(list, threshold);
+    }
+
+    public void assertCorrectSplit(Stream<String> stringStream, int firstIndexSecond) {
+        var list = stringStream.toList();
         var head = list.stream().collect(listItemCollector());
         var itemList = ListUtils.streamItems(head).toList();
         var n = itemList.size();
@@ -54,5 +65,31 @@ public class H2_2 {
         var list2StringActual = stream(headRight).collect(listToString());
         assertEquals(list1StringExpected, list1StringActual, format("first sequence differ for split(%s)", listString));
         assertEquals(list2StringExpected, list2StringActual, format("second sequence differ for split(%s)", listString));
+    }
+
+    public void assertAnchorAdaptiveMergeSortInPlace(Stream<String> stringStream, int threshold) {
+        var list = stringStream.collect(listItemCollector());
+        var listString = ListUtils.toString(list);
+        instance.useReferenceForSelectionSortInPlace();
+        instance.useReferenceForSplit();
+        instance.useRegerenceForMerge();
+        var callCount = new AtomicInteger(0);
+        doAnswer(invocation -> {
+                if (callCount.incrementAndGet() > 1) {
+                    var parameterListString = ListUtils.toString((ListItem<?>) invocation.getArgument(0));
+                    var parameterThreshold = (int) invocation.getArgument(1);
+                    return fail(
+                        format(
+                            "unexpected recursive call for adaptiveMergeSortInPlace(%s,%s): adaptiveMergeSortInPlace(%s,%s)",
+                            listString,
+                            threshold,
+                            parameterListString,
+                            parameterThreshold
+                        ));
+                }
+                return invocation.callRealMethod();
+            }
+        ).when(instance.student).adaptiveMergeSortInPlace(any(), anyInt());
+        instance.adaptiveMergeSortInPlace(list, threshold);
     }
 }
