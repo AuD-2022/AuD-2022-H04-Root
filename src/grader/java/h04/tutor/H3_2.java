8ff3cbe8e7a995c946be1e8f590003b9a5f057d4
Dustin Glaser <dustin.glaser@posteo.de>
2022-06-10 01:45:35 +0200

    add temporary tests for H3.2

---
+++ src/grader/java/h04/tutor/H3_2.java
@@ -0,0 +1,21 @@
+package h04.tutor;
+
+import h04.SortingExperiment;
+import h04.TUtils;
+import org.junit.jupiter.api.Test;
+
+public class H3_2 {
+
+    @Test
+    public void t1() {
+        TUtils.assertImplemented(() -> SortingExperiment.computeOptimalThresholds(10, 4, 3, 2));
+    }
+
+    @Test
+    public void t2() {
+        TUtils.assertImplemented(() -> {
+            SortingExperiment.main(new String[] {});
+            return null;
+        });
+    }
+}
